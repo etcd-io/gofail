0a5ef49dce16218ff782a59a5c492aad5c9e0c5a
Chun-Hung Tseng <henrybear327@gmail.com>
2024-05-14 22:51:52 +0200

    Add integration test
    Reference:
    - https://www.gnu.org/software/make/manual/html_node/MAKE-Variable.html
    
    Signed-off-by: Chun-Hung Tseng <henrybear327@gmail.com>
    Co-authored-by: Benjamin Wang <benjamin.wang@broadcom.com>

---
+++ integration/README.md
@@ -0,0 +1,5 @@
+# Integration Tests
+
+Each directory contains a scenario
+
+- sleep: the enabling and disabling of a failpoint won't be delayed due to an ongoing sleep() action
---
+++ integration/makefile.mk
@@ -0,0 +1,50 @@
+GOFAIL_BINARY = $(shell pwd)/gofail
+
+.PHONY: run-all-integration-tests
+run-all-integration-tests:
+	# we enable all failpoints
+	$(MAKE) gofail-enable
+
+	# we compile and execute all integration tests
+	# add new integration test targets here
+	$(MAKE) run-integration-test-sleep 
+
+	# we disable all failpoints
+	$(MAKE) gofail-disable
+	$(MAKE) clean-gofail
+
+.PHONY: clean-all-integration-tests
+clean-all-integration-tests: clean-integration-test-sleep gofail-disable
+
+.PHONY: gofail-enable
+gofail-enable: build-gofail
+	$(GOFAIL_BINARY) enable ./integration/sleep/failpoints
+
+.PHONY: gofail-disable
+gofail-disable: build-gofail
+	${GOFAIL_BINARY} disable ./integration/sleep/failpoints
+
+# run integration test - sleep
+.PHONY: run-integration-test-sleep
+run-integration-test-sleep: build-integration-test-sleep execute-integration-test-sleep clean-integration-test-sleep
+
+.PHONY: build-integration-test-sleep
+build-integration-test-sleep:
+	cd ./integration/sleep && go build -o integration_test_sleep .
+
+.PHONY: execute-integration-test-sleep
+execute-integration-test-sleep:
+	cd ./integration/sleep && ./integration_test_sleep
+
+.PHONY: clean-integration-test-sleep
+clean-integration-test-sleep:
+	cd ./integration/sleep && rm integration_test_sleep
+
+# helper: build/remove gofail binaries
+.PHONY: build-gofail
+build-gofail:
+	GO_BUILD_FLAGS="-v" ./build.sh
+
+.PHONY: clean-gofail
+clean-gofail:
+	rm -f gofail
